5e4ea377364e172ca5721ab472fb481d2e46568b
hangyu <chkue0924@lawendtech.co.kr>
2025-07-14 09:11:57 +0900

    update meta and favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,29 @@ export const metadata: Metadata = {
     "TypeScript",
     "경력직 개발자",
   ],
+  openGraph: {
+    title: "최한규 - Frontend Developer",
+    description:
+      "Frontend Developer & Full-Stack Engineer with 6+ years of experience",
+    url: "https://react-resume-pi-three.vercel.app/", // 실제 배포 URL로 변경해주세요.
+    siteName: "최한규의 개발 이력서",
+    images: [
+      {
+        url: "/profile.jpeg", // public 폴더의 이미지 경로
+        width: 1200, // 이미지의 실제 너비
+        height: 630, // 이미지의 실제 높이
+        alt: "최한규 프로필 이미지",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "최한규 - Frontend Developer",
+    description:
+      "Frontend Developer & Full-Stack Engineer with 6+ years of experience",
+    images: ["/profile.jpeg"], // public 폴더의 이미지 경로
+  },
 };
 
 export default function RootLayout({
